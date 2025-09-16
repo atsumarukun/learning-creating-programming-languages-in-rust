7b48ad61e2ccc3eafef422da0ad2697bfc40a8f3
atsumarukun <atys.smz@gmail.com>
2025-09-17 04:49:07 +0900

    supports function calls.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,30 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+#[derive(Clone)]
+struct NativeOp(fn(&mut Vm));
+
+impl PartialEq for NativeOp {
+    fn eq(&self, other: &NativeOp) -> bool {
+        self.0 as *const fn() == other.0 as *const fn()
+    }
+}
+
+impl Eq for NativeOp {}
+
+impl std::fmt::Debug for NativeOp {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "NativeOp")
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum Value {
     Num(i32),
     Op(String),
     Sym(String),
     Block(Vec<Value>),
+    Native(NativeOp),
 }
 
 impl Value {
@@ -38,6 +56,7 @@ impl Value {
             Self::Num(i) => i.to_string(),
             Self::Op(s) | Self::Sym(s) => s.clone(),
             Self::Block(_) => "<Block>".to_string(),
+            Self::Native(_) => "<Native>".to_string(),
         }
     }
 }
@@ -51,9 +70,24 @@ struct Vm {
 
 impl Vm {
     fn new() -> Self {
+        let functions: [(&str, fn(&mut Vm)); 10] = [
+            ("+", add),
+            ("-", sub),
+            ("*", mul),
+            ("/", div),
+            ("<", lt),
+            ("if", op_if),
+            ("def", op_def),
+            ("puts", puts),
+            ("dup", dup),
+            ("exch", exch),
+        ];
         Self {
             stack: vec![],
-            vars: HashMap::new(),
+            vars: functions
+                .into_iter()
+                .map(|(name, fun)| (name.to_owned(), Value::Native(NativeOp(fun))))
+                .collect(),
             blocks: vec![],
         }
     }
@@ -61,10 +95,10 @@ impl Vm {
 
 macro_rules! impl_op {
     {$name:ident, $op:tt} => {
-        fn $name(stack: &mut Vec<Value>) {
-            let rhs = stack.pop().unwrap().as_num();
-            let lhs = stack.pop().unwrap().as_num();
-            stack.push(Value::Num((lhs $op rhs) as i32));
+        fn $name(vm: &mut Vm) {
+            let rhs = vm.stack.pop().unwrap().as_num();
+            let lhs = vm.stack.pop().unwrap().as_num();
+            vm.stack.push(Value::Num((lhs $op rhs) as i32));
         }
     };
 }
@@ -109,30 +143,40 @@ fn puts(vm: &mut Vm) {
     println!("{}", value.to_string());
 }
 
-fn eval<'src>(code: Value, vm: &mut Vm) {
+fn dup(vm: &mut Vm) {
+    let value = vm.stack.last().unwrap();
+    vm.stack.push(value.clone());
+}
+
+fn exch(vm: &mut Vm) {
+    let last = vm.stack.pop().unwrap();
+    let second = vm.stack.pop().unwrap();
+    vm.stack.push(last);
+    vm.stack.push(second);
+}
+
+fn eval(code: Value, vm: &mut Vm) {
     if let Some(top_block) = vm.blocks.last_mut() {
         top_block.push(code);
         return;
     }
-    match code {
-        Value::Op(ref op) => match op as &str {
-            "+" => add(&mut vm.stack),
-            "-" => sub(&mut vm.stack),
-            "*" => mul(&mut vm.stack),
-            "/" => div(&mut vm.stack),
-            "<" => lt(&mut vm.stack),
-            "if" => op_if(vm),
-            "def" => op_def(vm),
-            "puts" => puts(vm),
-            _ => {
-                let val = vm
-                    .vars
-                    .get(op)
-                    .expect(&format!("{op:?} is not a defined operation"));
-                vm.stack.push(val.clone());
+    if let Value::Op(ref op) = code {
+        let val = vm
+            .vars
+            .get(op)
+            .expect(&format!("{op:?} is not a defined operation"))
+            .clone();
+        match val {
+            Value::Block(block) => {
+                for code in block {
+                    eval(code, vm);
+                }
             }
-        },
-        _ => vm.stack.push(code.clone()),
+            Value::Native(op) => op.0(vm),
+            _ => vm.stack.push(val),
+        }
+    } else {
+        vm.stack.push(code.clone());
     }
 }
 
@@ -244,5 +288,18 @@ mod test {
             ),
             vec![Num(10)]
         );
+    }
+
+    #[test]
+    fn test_function() {
+        assert_eq!(
+            parse(
+                r#"
+            /double { 2 * } def
+            10 double
+            "#
+            ),
+            vec![Num(20)]
+        )
     }
 }
